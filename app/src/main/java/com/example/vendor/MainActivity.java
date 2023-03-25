884406bdb2abfdb122d7eace0bf86654a823e3c8
ekta agarwal <ektaag369@gmail.com>
2023-03-25 19:47:59 +0530

    image bug commit

--- app/src/main/java/com/example/vendor/MainActivity.java
+++ app/src/main/java/com/example/vendor/MainActivity.java
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button add_seeds,add_chemicals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        add_chemicals=findViewById(R.id.chemicalsbtn);
+        add_seeds=findViewById(R.id.seedsbtn);
 
+        add_seeds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,AddSeeds.class);
+                startActivity(i);
+            }
+        });
     }
 }
